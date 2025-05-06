930101b15622a132ac82972ae3c417aae7f32e1e
Daniel Chifamba <115432944+danielChifamba@users.noreply.github.com>
2025-05-06 13:35:01 +0200

    Update settings.py

--- Vidvibe/settings.py
+++ Vidvibe/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['vidvibe.up.railway.app', '127.0.0.1', 'localhost']
 
 CSRF_TRUSTED_ORIGIN = [
-    "https://vidvibe.up.railway.app",
+    "http://vidvibe.up.railway.app",
 ]
 
 CSRF_COOKIE_SECURE = True
